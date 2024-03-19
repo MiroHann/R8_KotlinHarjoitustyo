60a25d758cfab247f88e65f5e4ae75c993d46305
MiroHann <t1hami03@students.oamk.fi>
2024-03-19 20:26:39 +0200

    Image on click funtionality that takes you to the recipe page that contains info on the dish

--- app/src/main/java/com/example/apiparser/RecipeModels.kt
+++ app/src/main/java/com/example/apiparser/RecipeModels.kt
@@ -1,14 +1,22 @@
 package com.example.apiparser
 
+import org.json.JSONArray
+import org.json.JSONObject
+import java.lang.reflect.Array
+
 data class RecipeResponse(
     val hits: List<RecipeHit>
 )
+data class Ingredientlist(
+    val ingredients: String
+)
 
 data class RecipeHit(
-    val recipe: Recipe
+    val recipe: Recipe,
 )
 
 data class Recipe(
     val label: String,
-    val image: String
+    val image: String,
+    val ingredientLines: List<String>
 )
--- app/src/main/java/com/example/apiparser/recipe.kt
+++ app/src/main/java/com/example/apiparser/recipe.kt
@@ -3,44 +3,39 @@ package com.example.apiparser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+class RecipePage : AppCompatActivity() {
 
-class Recipe : AppCompatActivity() {
-
-
+  private var arrayInstructions : Array<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intentValues = intent.getStringExtra("TitleText")
-        val recipeText = intent.getStringExtra("RecipeText").toString()
+        arrayInstructions  = intent.getStringArrayExtra("RecipeText")
+        val imageURL = intent.getStringExtra("Image").toString()
         setContent {
             title = intentValues
             MaterialTheme{
-                    RecipeText(content = recipeText)
+                    RecipeText(image = imageURL)
             }
         }
     }
     @Composable
-    fun RecipeText(content: String, modifier: Modifier = Modifier) {
+    fun RecipeText(image: String, modifier: Modifier = Modifier) {
         Column(
             modifier = modifier
                 .fillMaxHeight()
@@ -49,18 +44,23 @@ class Recipe : AppCompatActivity() {
             horizontalAlignment = Alignment.CenterHorizontally
 
         ) {
-            Text(
-                text = "Header",
-                modifier = modifier.fillMaxWidth().padding(top = 62.dp),
-                textAlign = TextAlign.Center,
-            )
-            AsyncImage(model = "https://upload.wikimedia.org/wikipedia/en/c/c2/Peter_Griffin.png", contentDescription = "Placeholder")
-            Text(
-                text = content,
-                modifier = modifier.fillMaxWidth().padding(top = 62.dp),
-                textAlign = TextAlign.Center,
-            )
-            
+            AsyncImage(model = image, contentDescription = "Placeholder")
+            LazyColumn(
+                modifier = modifier
+            ) {
+                arrayInstructions?.let {
+                    items(it.toMutableList()){ content ->
+                        Text(
+                            text = content,
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(top = 62.dp),
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+            }
         }
     }
+
 }
